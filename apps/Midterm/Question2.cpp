dbaa9d0e20e0ff427e7f4a79fcc287dd4676c356
BUpton3 <156273667+BUpton3@users.noreply.github.com>
2024-10-01 19:49:12 -0500

    Create Question2.cpp

---
+++ apps/Midterm/Question2.cpp
@@ -0,0 +1,12 @@
+// Breah Upton 00087865 bupton3@my.athens.edu
+// Used For Help: https://cs226fa22.github.io/notes/22-hashing/step11.html#:~:text=The%20solution%20is%20lazy%20deletion:%20flag%20the%20item%20as%20deleted
+// Used For Help: https://www.geeksforgeeks.org/cpp-program-for-searching-an-element-in-a-linked-list/
+// Used For Help: https://www.geeksforgeeks.org/delete-occurrences-given-key-linked-list/
+
+#include <iostream>
+#include "ListClass.hpp"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
