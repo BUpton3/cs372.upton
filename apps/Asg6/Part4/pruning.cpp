c50449ceef404fc0f03dc5aa474e83cac9412ce9
Breah Upton <156273667+BUpton3@users.noreply.github.com>
2024-11-15 14:01:19 -0600

    Create pruning.cpp

---
+++ apps/Asg6/Part4/pruning.cpp
@@ -0,0 +1,31 @@
+#include "treePruning.hpp"
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    Tree<int> tree;
+    vector<int> aTree({ 45,74,28,32,45,44, 100 });
+    for (auto& it : aTree) {
+        tree.insertNode(it);
+    }
+    cout << "Inorder traversal: ";
+    tree.inorder();
+
+    cout << "Preorder traversal: ";
+    tree.preorder();
+
+    cout << "Postorder traversal: ";
+    tree.postorder();
+
+    cout << "Deleting 28 from Tree. \n";
+    tree.deleteNode(28);
+    cout << "Inorder traversal after deletion: ";
+    tree.inorder();
+
+    tree.pruningLead();
+    tree.inorder();
+    
+
+    return 0;
+}
