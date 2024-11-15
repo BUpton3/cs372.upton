ea0284452cf6a6cac0de7f34f00f890d9f1c7fb4
Breah Upton <156273667+BUpton3@users.noreply.github.com>
2024-11-15 13:36:44 -0600

    Create dictionary.cpp

---
+++ apps/Asg6/Part5/dictionary.cpp
@@ -0,0 +1,22 @@
+// Used for help: https://www.geeksforgeeks.org/map-associative-containers-the-c-standard-template-library-stl/
+// Used for help: https://www.geeksforgeeks.org/how-to-create-a-dictionary-in-cpp/
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+
+    // Create an empty dictionary using std::map
+    map<string, string> dict;
+
+    // Insert key-value pairs
+    dict["Hockey"] = "A winter sport played on ice with sticks and rubber pucks.";
+    dict["Books"] = "Bound paper which contains printed matter.";
+    dict["Legos"] = "A popular plastic building toy.";
+
+    for (auto it : dict)
+        cout << it.first << ": " << it.second << endl;
+
+    return 0;
+}
