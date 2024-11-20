0291826070d35add365007aab679f1dc4e44ce24
Breah Upton <156273667+BUpton3@users.noreply.github.com>
2024-11-19 18:48:31 -0600

    Create Q1.cpp

---
+++ apps/finalExam/ProgrammingComponent/Q1.cpp
@@ -0,0 +1,29 @@
+// treeFunctions.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include "Tree.hpp"
+using namespace std;
+int main()
+{
+    Tree<int> tree;
+    vector<int> aTree({ 45,74,28,32,45,44, 100 });
+    for (auto& it : aTree) {
+        tree.insertNode(it);
+    }
+
+    // counting the Nodes
+    // used for help: https://www.geeksforgeeks.org/count-number-of-nodes-in-a-complete-binary-tree/
+    tree.nodeCount();
+
+    // counting internal nodes
+    // used for help: https://www.geeksforgeeks.org/print-all-internal-nodes-of-a-binary-tree/
+    tree.internalNodeCount();
+
+    // external path length
+    // used for help: Microsoft Bing AI programming tool. Specific search was "c++ how to add up external nodes in a tree"
+    tree.externalPath();
+
+    return 0;
+}
